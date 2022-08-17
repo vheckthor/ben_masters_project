6509631f43daba775b5df4af754a5c41f306abd1
Dare Victor Adebayo <43048158+vheckthor@users.noreply.github.com>
2022-08-17 21:51:49 +0100

    added env data

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ey*vm=y7#x2zqv2xqkcow6dcvt-vynlaxl1y&gje4vh$xy6!_('
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',"")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
